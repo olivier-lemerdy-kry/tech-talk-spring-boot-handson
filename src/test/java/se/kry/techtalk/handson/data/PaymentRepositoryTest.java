c1f31b2f5184a1f6f05e51c0797435ee79425a35
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-01-19 11:13:26 +0100

    Better Constraint Violation assertion

--- src/test/java/se/kry/techtalk/handson/data/PaymentRepositoryTest.java
+++ src/test/java/se/kry/techtalk/handson/data/PaymentRepositoryTest.java
@@ -3,7 +3,8 @@ package se.kry.techtalk.handson.data;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import jakarta.validation.ValidationException;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -52,7 +53,15 @@ class PaymentRepositoryTest {
     repository.save(toSave);
 
     assertThatThrownBy(() -> testEntityManager.flush())
-        .isInstanceOf(ValidationException.class);
+        .isInstanceOfSatisfying(ConstraintViolationException.class, exception -> {
+          assertThat(exception.getConstraintViolations())
+              .hasSize(1)
+              .first()
+              .satisfies(violation -> {
+                assertThat(violation.getPropertyPath()).hasToString("amountCurrency");
+                assertThat(violation.getMessageTemplate()).isEqualTo("{" + NotNull.class.getName() + ".message}");
+              });
+        });
   }
 
 }
